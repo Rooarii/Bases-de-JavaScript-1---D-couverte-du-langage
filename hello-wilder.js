412cb60155ddb70af035d5fb0be21c239ada18f0
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-09-21 08:51:45 +0200

    code refactoring

--- hello-wilder.js
+++ hello-wilder.js
@@ -2,6 +2,6 @@
 
 const favoriteMovie='Braveheart';
 const director='Mel GIBSON';
-const releaseDate='October 4, 1995';
+const releaseYear=1995;
 
-alert(favoriteMovie +', directed by '+director+ ' was released in '+ releaseDate);
+alert(favoriteMovie +', directed by '+director+ ' was released in '+ releaseYear);
